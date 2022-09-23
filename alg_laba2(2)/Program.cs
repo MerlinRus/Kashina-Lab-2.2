f555403dd03733ebcb0d010a252a34b39b7df520
MARK <MARK@DESKTOP-CU8IC72>
2022-09-23 10:41:13 +0300

    Добавьте файлы проекта.

---
+++ alg_laba2(2)/Program.cs
@@ -0,0 +1,17 @@
+﻿const int sizeArray = 3;
+int[] a = new int[sizeArray];
+
+Random r = new Random();
+for (int i = 0; i < sizeArray; ++i)
+{
+    a[i] = r.Next(-50, 100);
+}
+
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < sizeArray; ++i)
+{
+    Console.WriteLine("\t" + a[i]);
+}
+
+int min = a.Min(t => Math.Abs(t));
+Console.WriteLine("Минимальный элемент = {0}", min, Math.Abs(min));
